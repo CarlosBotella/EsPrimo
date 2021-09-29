70855334eccd8599de2cf6b0c6a0fc185637504a
cbotella <carlosb.n@icloud.com>
2021-09-29 13:37:08 +0200

    final commit esPrimo

--- EsPrimo/main.cpp
+++ EsPrimo/main.cpp
@@ -10,17 +10,36 @@ using namespace std;
 
 bool esPrimo(){
     bool comparativo;
-    comparativo = false;
-    int n;
-    cout << "Elige un número ";
+    comparativo = false; // inicializado
+    int n; // inicializado
+    int i;
+    int total = 0;
+    cout << "Elige un número entero: ";
     cin >> n;
-    if ((n%1 == 0) && (n%n == 0)) {
+    for (i = 1; i<=n; i=i+1) {
+        int remainder;
+//        cout << "i: " << i << "\n";
+        remainder = n%i;
+//        cout << remainder << "\n";
+        if (remainder == 0 ) {
+            total = total + 1;
+            
+    if (total == 2) {
         comparativo = true;
     }
-    cout << comparativo;
+    else {
+        comparativo = false;
+            }
+        }
+    }
+//    cout << comparativo << "\n";
     return comparativo;
 }
 
 int main(){
-    esPrimo();
+    if (esPrimo()==true) {
+        cout << "el número es primo" << "\n";
+    } else {
+        cout << "el número no es primo" << "\n";
+    }
 }
