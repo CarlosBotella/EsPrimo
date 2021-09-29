99e3e06fd6708b34d05a8c6b04195592e863aeab
cbotella <carlosb.n@icloud.com>
2021-09-29 12:38:55 +0200

    Initial Commit

---
+++ EsPrimo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EsPrimo
+//
+//  Created by Carlos Botella Navarro on 29/9/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
